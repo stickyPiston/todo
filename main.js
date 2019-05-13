4582fb449c6d4bb308bf2d6982f493b9001cb275
Job <job@jobbel.nl>
2019-05-13 19:08:14 +0200

    Initial commit
    loaded all files

---
+++ main.js
@@ -0,0 +1,95 @@
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+const isDev = require('electron-is-dev')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 400,
+    height: 100,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // Create menu template
+  var menu = Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin' ? 'CommandOrControl+Option+I' : 'CommandOrControl+Alt+I',
+          click () {
+            win.webContents.toggleDevTools()
+          }
+        },
+        { type: 'separator' },
+        {
+          label: 'Quit',
+          accelerator: 'CmdOrCtrl+Q',
+          click () {
+            app.quit()
+          }
+        }
+      ]
+    }
+  ])
+  Menu.setApplicationMenu(menu)
+
+  // and load the index.html of the app.
+  win.loadFile('src/index.html')
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+
+  ipcMain.on('heightChanged', (_event, arg) => {
+    if (win.getSize()[1] >= (arg * 29 + 105)) {
+      // Nothing ought to happen
+    } else {
+      win.setSize(win.getSize()[0], (arg * 29 + 105), true)
+    }
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  createWindow()
+})
+
+if (isDev) {
+  app.setLoginItemSettings({
+    openAtLogin: true,
+    args: [__dirname]
+  })
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
---
+++ src/index.js
@@ -0,0 +1,97 @@
+const fs = require('fs')
+const path = require('path')
+const { ipcRenderer } = require('electron')
+
+var input = document.getElementById('input')
+var list = document.getElementById('list')
+
+function createItem (text, done) {
+  var newNode = document.createElement('li')
+  var spanNode = document.createElement('span')
+  var checkboxNode = document.createElement('input')
+  checkboxNode.setAttribute('type', 'checkbox')
+  var textNode = document.createTextNode(text)
+  var separator = document.createElement('span')
+  var separatorTextNode = document.createTextNode(' ')
+
+  separator.appendChild(separatorTextNode)
+  separator.setAttribute('class', 'seperator')
+
+  var deleteBtn = document.createElement('button')
+  var deleteText = document.createTextNode('Delete')
+  deleteBtn.setAttribute('class', 'delete')
+  deleteBtn.appendChild(deleteText)
+
+  deleteBtn.addEventListener('click', e => {
+    e.target.parentElement.parentElement.removeChild(e.target.parentElement)
+    ipcRenderer.send('heightChanged', document.getElementsByTagName('li').length)
+    SaveList()
+  })
+
+  spanNode.appendChild(textNode)
+
+  checkboxNode.setAttribute('title', 'Check this to mark an item as done')
+  checkboxNode.addEventListener('click', e => {
+    if (!e.target.checked) e.target.parentElement.style.textDecoration = 'none'
+    else e.target.parentElement.style.textDecoration = 'line-through'
+
+    SaveList()
+  })
+
+  checkboxNode.checked = done
+  newNode.appendChild(checkboxNode)
+  newNode.appendChild(spanNode)
+  newNode.appendChild(separator)
+  newNode.appendChild(deleteBtn)
+  newNode.style.textDecoration = done ? 'line-through' : 'none'
+  list.appendChild(newNode)
+}
+
+function ListSave () {
+  var save = fs.readFileSync(path.join(__dirname, 'save.json'))
+  save = JSON.parse(save.toString())
+  save.forEach(element => {
+    createItem(element.data, element.done)
+  })
+}
+
+function SaveList () {
+  var data = []
+
+  Array.from(document.querySelectorAll('li > span:not(.seperator)')).forEach(element => {
+    var done = element.parentElement.style.textDecoration === 'line-through'
+    data.push({ 'data': element.innerText, 'done': done })
+  })
+
+  data = JSON.stringify(data)
+
+  fs.writeFileSync(path.join(__dirname, 'save.json'), data, (err) => {
+    if (err) throw err
+  })
+}
+
+function EmptyList () {
+  var list = document.getElementById('list')
+
+  list.innerHTML = ''
+}
+
+document.getElementsByTagName('form')[0].addEventListener('submit', e => {
+  e.preventDefault()
+  EmptyList()
+
+  // Amend the save.json file as well.
+  ListSave()
+
+  createItem(input.value)
+
+  ipcRenderer.send('heightChanged', document.getElementsByTagName('li').length)
+
+  input.value = ''
+
+  SaveList()
+})
+
+ListSave()
+
+ipcRenderer.send('heightChanged', document.getElementsByTagName('li').length)
